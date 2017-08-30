26aa77c626749a1a0773abd5962ecfdffd0e9338
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-08-30 09:40:28 +0200

    add repetition prevention

--- piano.js
+++ piano.js
@@ -1,9 +1,45 @@
+var down = {
+    "z": false,
+    "s": false,
+    "x": false,
+    "d": false,
+    "c": false,
+    "v": false,
+    "g": false,
+    "b": false,
+    "h": false,
+    "n": false,
+    "j": false,
+    "m": false,
+    "q": false,
+    "2": false,
+    "w": false,
+    "3": false,
+    "e": false,
+    "r": false,
+    "5": false,
+    "t": false,
+    "6": false,
+    "y": false,
+    "7": false,
+    "u": false,
+    "i": false,
+    "9": false,
+    "o": false,
+    "0": false,
+    "p": false
+};
+
 window.addEventListener("keydown", function (event) {
-	console.log("DOWN: " + event.key + "  " + event.keyCode);
+	if(!down[event.key]) {
+		down[event.key] = true;
+		console.log("DOWN: " + event.key + "  " + event.keyCode);
+	}
 	// sounds[0].play();
 }, false);
 
 window.addEventListener("keyup", function (event) {
+	down[event.key] = false;
 	console.log("  UP: " + event.key + "  " + event.keyCode);
 	// sounds[0].play();
 }, false);
