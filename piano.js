564506766e157861b840ab83f1ba60f5b1403431
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-08-30 10:05:56 +0200

    connect keys to sounds

--- piano.js
+++ piano.js
@@ -29,18 +29,49 @@ var down = {
     "0": false,
     "p": false
 };
+const keys = {
+    "z": 1,
+    "s": 2,
+    "x": 3,
+    "d": 4,
+    "c": 5,
+    "v": 6,
+    "g": 7,
+    "b": 8,
+    "h": 9,
+    "n": 10,
+    "j": 11,
+    "m": 12,
+    "q": 13,
+    "2": 14,
+    "w": 15,
+    "3": 16,
+    "e": 17,
+    "r": 18,
+    "5": 19,
+    "t": 20,
+    "6": 21,
+    "y": 22,
+    "7": 23,
+    "u": 24,
+    "i": 25,
+    "9": 26,
+    "o": 27,
+    "0": 28,
+    "p": 29
+};
 
 window.addEventListener("keydown", function (event) {
 	if(!down[event.key]) {
 		down[event.key] = true;
-		console.log("DOWN: " + event.key + "  " + event.keyCode);
+		if(keys[event.key]){
+			sounds[keys[event.key]].play();
+		}
 	}
-	// sounds[0].play();
 }, false);
 
 window.addEventListener("keyup", function (event) {
 	down[event.key] = false;
-	console.log("  UP: " + event.key + "  " + event.keyCode);
 	// sounds[0].play();
 }, false);
 
