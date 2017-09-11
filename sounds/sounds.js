cf621c31ccb52abdd4183496e4ad61f39ae6d17f
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-09-11 09:52:06 +0200

    move keys to piano

--- sounds/sounds.js
+++ sounds/sounds.js
@@ -227,6 +227,38 @@ function createPiano() {
         "p": false
     }
 
+    const keys = {
+        "z": 1,
+        "s": 2,
+        "x": 3,
+        "d": 4,
+        "c": 5,
+        "v": 6,
+        "g": 7,
+        "b": 8,
+        "h": 9,
+        "n": 10,
+        "j": 11,
+        "m": 12,
+        "q": 13,
+        "2": 14,
+        "w": 15,
+        "3": 16,
+        "e": 17,
+        "r": 18,
+        "5": 19,
+        "t": 20,
+        "6": 21,
+        "y": 22,
+        "7": 23,
+        "u": 24,
+        "i": 25,
+        "9": 26,
+        "o": 27,
+        "0": 28,
+        "p": 29
+    }
+    
     var octave = 1
     var octaveHTML = document.getElementById("octave")
 
--- todo.md
+++ todo.md
@@ -1,2 +1,1 @@
-- show octave
 - space bar as pedal
